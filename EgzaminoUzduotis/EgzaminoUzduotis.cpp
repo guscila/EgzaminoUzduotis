46240a8ae130113bfd8669d71e9d691634f5203a
guscila <guscila@OKI>
2025-12-18 19:39:12 +0200

    sutvarkytos lt raidės ir skyryba bei atliktas antras punktas

--- EgzaminoUzduotis/EgzaminoUzduotis.cpp
+++ EgzaminoUzduotis/EgzaminoUzduotis.cpp
@@ -4,13 +4,14 @@
 #include <string>
 #include <map>
 #include <cctype>
+#include <set>
 
 //#include "funkcijos.cpp"
 
 using namespace std;
 
-bool SimbolioTestas(unsigned char simb) {
-    return isalnum(simb) || simb >= 128;
+bool SimboliuFiltras(unsigned char simb) {
+    return isalnum(simb);
 }
 
 char ZodziuTransformacija(char raide) {
@@ -28,33 +29,54 @@ int main() {
         return 0;
     }
     map<string, int> zodziai;
+    map<string, set<int>> cross;
     string eil, zodis;
+    int vieta = 0;
+    bool LtRaides = false;
     while (getline(df, eil)) {
+        vieta++;
         for (unsigned char charas : eil) {
-            if (SimbolioTestas(charas)) {
+            if (SimboliuFiltras(charas)) {
                 char simb = (char)charas;
                 if (charas < 128) simb = ZodziuTransformacija(simb);
                 zodis.push_back(simb);
+                LtRaides = false;
+            }
+            else if (charas == 0xC4 || charas == 0xC5) {
+                zodis.push_back((char)charas);
+                LtRaides = true;
+            }
+            else if (LtRaides && (charas >= 0x80 && charas <= 0xBF)) {
+                zodis.push_back((char)charas);
+                LtRaides = false;
             }
             else {
+                LtRaides = false;
                 if (!zodis.empty()) {
                     zodziai[zodis]++;
+                    cross[zodis].insert(vieta);
                     zodis.clear();
                 }
             }
         }
         if (!zodis.empty()) {
             zodziai[zodis]++;
+            cross[zodis].insert(vieta);
             zodis.clear();
         }
     }
     ofstream rf("rezultatai.txt");
-    rf << left << setw(15) << "Zodis" << setw(15) << "Kartai" << endl;
+    rf << left << setw(20) << "Zodis" << setw(15) << "Kartai" << "Lokacija (eil. nr.)" << endl;
     for (const auto& obj : zodziai) {
         const string& zodis = obj.first;
         int kiekis = obj.second;
         if (kiekis > 1) {
-            rf << zodis << " : " << kiekis << "\n";
+            rf << left << setw(20) << zodis << setw(15) << kiekis;
+            const set<int>& eilute = cross[zodis];
+            for (int i : eilute) {
+                rf << i << ", ";
+            }
+            rf << endl;
         }
     }
     cout << "Rezultatai isvesti faile 'rezultatai.txt'.\n";
